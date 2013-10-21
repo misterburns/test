cd096c48a2977369bf2d4c4d64e665b48134a518
Bernd Wegscheider <wegscheider@proartist.at>
2013-10-21 15:50:02 +0200

    ok

--- test.php
+++ test.php
@@ -5,4 +5,9 @@ echo "ok";
 echo "ok";echo "ok";
 =======
 >>>>>>> parent of 64ccff2... 3rd line
+echo "ok";<<<<<<< HEAD
+echo "ok";
+echo "ok";echo "ok";
+=======
+>>>>>>> parent of 64ccff2... 3rd line
 echo "ok";
